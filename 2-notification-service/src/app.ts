7f8823f9bde7f82fde13911a62b159c96682f42c
ahmedhamdy <ahmedhamdyz22435@gmail.com>
2025-11-26 04:53:33 +0200

    Fix package entry points and bump version

--- 2-notification-service/src/app.ts
+++ 2-notification-service/src/app.ts
@@ -1,1 +1,16 @@
+import { winstonLogger } from "@3brazek234/ejad-shared";
+import { Logger } from "winston";
+import { config } from "./config";
+import express, { Express } from "express";
+import { start } from "./server";
 // start of application
+const log: Logger = winstonLogger(
+  `${config.ELASTIC_SEARCH_URL}`,
+  "notification-server",
+  "debug"
+);
+function initialize(): void {
+  const app: Express = express();
+  start(app);
+  log.info("Notification service initialized");
+}
